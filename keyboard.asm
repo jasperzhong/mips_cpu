18c35945f6bd2ec13c4efbd1b26eaa0c3b8d25ac
zhongyuchen <izhongyuchen@gmail.com>
2018-07-20 19:17:38 +0800

    keyboard

--- keyboard.asm
+++ keyboard.asm
@@ -5,11 +5,18 @@ lui $11, 0x1001
 
 
 addi $22, $22, 0   # b = 0
-query:
-lb $21, 0($10)     #  while( cin >> a )
-beq $21, $22, query # if (a == b) continue 
+addi $23, $23, 0
+addi $24, $24, 0
 
-sw $21, 0($11)     # cout << a
-sll $21, $21, 4    # a = (a << 4)
-add $22, $21, $0   # b = a
+query:
+lw $21, 0($10)     #  while( cin >> a )
+sw $22, 0($11)     #  cout << b
+
+beq $21, $0, query # if ( a == 0) continue 
+beq $21, $24,query
+addi $24, $21, 0
+
+or $21, $21, $22   
+sllv $22, $21, $23   # b = (a << v)
+addi $23, $23, 8  # v += 8
 j query 
